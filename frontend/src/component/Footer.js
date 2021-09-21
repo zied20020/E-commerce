7be4517c12a140a8bfef8c855e9c096cd4c7f37a
zied20020 <ziedjlidi@gmail.com>
2021-09-21 11:15:45 +0100

    first commit

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='d-flex justify-content-center py-5 mt-5'>
-        <a href='https://www.pakkamarwadi.tk/' target='_blank'>
+        <a href='https://gomycode.com/' target='_blank'>
           <i class='fas fa-code'></i> with <i class='fas fa-heart'></i>{" "}
-          Pakkamarwadi
+          GoMyCode
         </a>
       </div>
     );
